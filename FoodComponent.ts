cf411582d54799493d8a8f735cd91b94a0d2e60f
Eduardo Morales <AzPD5SUKpqVt>
2020-04-30 18:18:55 -0400

    remove food component

--- FoodComponent.ts
+++
@@ -1,9 +0,0 @@
-export default class Food {
-  position: {row: number, column: number} = {row: 0, column: 0}
-  constructor(rows: number, columns: number) {
-    this.position = {
-      row: Math.floor(Math.random() * rows),
-      column: Math.floor(Math.random() * columns)
-    }
-  }
-}
